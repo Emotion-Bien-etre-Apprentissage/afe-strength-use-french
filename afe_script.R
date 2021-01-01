bedb1a638de8bfd4dbc4ba31e30f3300f829caf8
Nicolas Bressoud <65454893+bresnico@users.noreply.github.com>
2021-01-01 16:21:17 +0100

    Update afe_script.R

--- afe_script.R
+++ afe_script.R
@@ -17,9 +17,4 @@ convert("data/bdd_master.xlsx", "data/quebec2020_jf_master.csv")
 
 # suite du code----
 
-<<<<<<< Updated upstream
-truc.
-=======
 source("src/anal_val_int_v.2.R")
-
->>>>>>> Stashed changes
