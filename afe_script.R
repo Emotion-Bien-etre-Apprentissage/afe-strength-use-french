ad89619085772e60a56bbf2bc1af7fa50f35ce82
Nicolas Bressoud <65454893+bresnico@users.noreply.github.com>
2021-01-01 14:29:22 +0100

    Acquisition des bdd et conversion

---
+++ afe_script.R
@@ -0,0 +1,18 @@
+# Chargement des libraries----
+
+library(tidyverse) # manipulation selon les règles tidy
+library(rio) # pour la conversion sav <-> csv ou/et xlsx
+
+# Acquisition des 6 bdd et conversion----
+# en xlsx pour gagner du temps dans la manipulation
+
+for (i in 1:6) {
+  a <- paste("data/bdd",i,".sav", sep = "") # fichiers reçus par J. Forest
+  b <- paste("data/bdd",i,".xlsx", sep = "") # fichiers préparés pour être compilés à la main.
+  convert(a,b)
+}
+
+# Conversion du Master xlsx -> csv
+convert("data/bdd_master.xlsx", "data/quebec2020_jf_master.csv")
+
+# suite du code----
